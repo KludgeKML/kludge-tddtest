b2d43825d349f521b56b9645819d7b02f00978e4
Keith Lawrence <keith@kludge.co.uk>
2013-09-27 19:37:34 +0100

    Moved URL rules to lists/urls.py

--- lists/urls.py
+++ lists/urls.py
@@ -6,8 +6,9 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/', include('lists.urls')),
+    url(r'^(.+)/$', 'lists.views.view_list', name='view_list'),
+    url(r'^(.+)/new_item$', 'lists.views.add_item', name='add_item'),
+    url(r'^new$', 'lists.views.new_list', name='new_list'),
     # url(r'^superlists/', include('superlists.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
